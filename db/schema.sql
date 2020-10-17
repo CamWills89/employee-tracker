89e4760dbd048c20bbd2a8a16ca3d7704c67a367
Cameron <cameron.wills89@outlook.com>
2020-10-17 16:33:10 -0400

    another failed attempt

--- db/schema.sql
+++ db/schema.sql
@@ -17,8 +17,7 @@ CREATE TABLE employee(
   first_name VARCHAR(30) NOT NULL,
   last_name VARCHAR(30) NOT NULL,
   role_id INTEGER,
-  manager_id INTEGER,
-  INDEX man_ind (manager_id),
   CONSTRAINT fk_role FOREIGN KEY (role_id) REFERENCES role(id) ON DELETE CASCADE,
-  CONSTRAINT fk_manager FOREIGN KEY (manager_id) REFERENCES employee(id) ON DELETE SET NULL
+  manager_id INTEGER,
+  INDEX man_ind (manager_id)
 );
